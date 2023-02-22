84a512fad689f8909b082575d364c30bd4261192
msteiner <msteiner@christ-ps.com>
2023-02-22 08:47:44 +0100

    improve Scroll, prevent event bubbling

--- proControl/lib/ui/layout/Scroll/Scroll.tsx
+++ proControl/lib/ui/layout/Scroll/Scroll.tsx
@@ -6,7 +6,17 @@ import { ScrollProps } from "./ScrollProps"
 export const Scroll = forwardRef<HTMLDivElement, ScrollProps>((props, ref) => {
   return (
     <Relative s="100%" {...props} ref={ref}>
-      <Absolute top="0" right="0" bottom="0" left="0" css={{ overflow: "auto" }}>
+      <Absolute
+        top="0"
+        right="0"
+        bottom="0"
+        left="0"
+        css={{ overflow: "auto" }}
+        onPointerMove={(e) => {
+          e.preventDefault()
+          e.stopPropagation()
+        }}
+      >
         {props.children}
       </Absolute>
     </Relative>
