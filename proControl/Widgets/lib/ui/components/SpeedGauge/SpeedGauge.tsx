431e19b3508a57db43d1bab059ef6db19b06f3b0
msteiner <msteiner@christ-ps.com>
2023-02-23 11:15:42 +0100

    put a Box around the component, becose Relative do not handle all clicks

--- proControl/Widgets/lib/ui/components/SpeedGauge/SpeedGauge.tsx
+++ proControl/Widgets/lib/ui/components/SpeedGauge/SpeedGauge.tsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from "react"
 import { Line, Rect, Svg, Circle, G, Text } from "@proControl/lib/ui/svg"
-import { Absolute, Relative, Center, Stack } from "@proControl/lib/ui/layout"
+import { Absolute, Relative, Center, Stack, Box } from "@proControl/lib/ui/layout"
 import {
   calculateCircumference,
   scale,
@@ -56,9 +56,11 @@ export const SpeedGauge = ({
   }, [hasSlider, sliderStartValue])
 
   return (
-    <Relative
-      s="100%"
+    <Box
       onPointerMove={(e) => {
+        //dont, handle, if we dont have a slider
+        if (!hasSlider) return
+
         //only handle touch
         if (e.pointerType === "mouse") return
 
@@ -104,120 +106,122 @@ export const SpeedGauge = ({
         userSelect: "none"
       }}
     >
-      {/** Text */}
-      {!debug && (
-        <Absolute top="0px" right="0px" bottom="0px" left="0px">
-          <Center h="100%">
-            <Stack g="$sm">
-              <Center fs="$3xl" fw="$bold" c={color}>
-                {value.toLocaleString(locale, {
-                  maximumFractionDigits: decimalPlaces,
-                  minimumFractionDigits: decimalPlaces
-                })}
-              </Center>
-              {unit && <Center>[{unit}]</Center>}
-            </Stack>
-          </Center>
-        </Absolute>
-      )}
-
-      {/** Graphic */}
-      <Svg width="100%" height="100%" fill="none" viewBox="-100,-100,200,200" ref={svgRef}>
-        {/** value */}
-        <Circle
-          x={0}
-          transform="rotate(-90)"
-          y={0}
-          r={RADIUS}
-          strokeWidth={WIDTH}
-          css={{ stroke: color, transitionDuration: "600ms" }}
-          strokeDasharray={CIRCUMFERENCE}
-          strokeDashoffset={scale(clamp(value, min, max), [min, max], [CIRCUMFERENCE, 0])}
-        />
-
-        {/** label marks */}
-        {calculateStepLabel(min, max, steps, 0, 360).map(({ pos }, index) => {
-          //dont print first element, if first and last el has same pos
-          if (
-            (steps % 5 === 0 || steps % 10 === 0 || steps === 0.5 || steps === 0.1) &&
-            index === 0
-          )
-            return null
-
-          //print labels
-          return (
-            <Rect
-              key={index}
-              x={0}
-              y={0}
-              width={LABEL_WIDTH}
-              height={LABEL_LENGTH}
-              transform={`rotate(${pos}) translate(-${LABEL_WIDTH / 2},-${
-                RADIUS + LABEL_LENGTH / 2 + WIDTH + GAP
-              })`}
-              fill="gray"
-            />
-          )
-        })}
-
-        {/** Slider / Handle*/}
-        {hasSlider && (
-          <G
-            transform={`rotate(${scale(
-              snapToStep(scale(targetAngle, [0, 360], [min, max]), [0, max], steps),
-              [min, max],
-              [0, 360]
-            )}), translate(0,-86)`}
-          >
-            <Circle x={0} y={0} r={10} css={{ fill: "rgba(0,0,0,0.7)" }} />
-            {debug && (
-              <Line x1={0} y1={0} x2={0} y2={32} css={{ stroke: "purple", strokeWidth: 2 }} />
-            )}
-          </G>
+      <Relative s="100%">
+        {/** Text */}
+        {!debug && (
+          <Absolute top="0px" right="0px" bottom="0px" left="0px">
+            <Center h="100%">
+              <Stack g="$sm">
+                <Center fs="$3xl" fw="$bold" c={color}>
+                  {value.toLocaleString(locale, {
+                    maximumFractionDigits: decimalPlaces,
+                    minimumFractionDigits: decimalPlaces
+                  })}
+                </Center>
+                {unit && <Center>[{unit}]</Center>}
+              </Stack>
+            </Center>
+          </Absolute>
         )}
 
-        {/**Debug */}
-        {debug && (
-          <>
-            <Rect x={0} y={0} width={4} height={4} transform="translate(-2,-2)" fill="hotpink" />
-            <Line x1={-100} x2={100} y1={0} y2={0} stroke="hotpink" strokeWidth={0.5} />
-            <Line x1={0} x2={0} y1={-100} y2={100} stroke="hotpink" strokeWidth={0.5} />
+        {/** Graphic */}
+        <Svg width="100%" height="100%" fill="none" viewBox="-100,-100,200,200" ref={svgRef}>
+          {/** value */}
+          <Circle
+            x={0}
+            transform="rotate(-90)"
+            y={0}
+            r={RADIUS}
+            strokeWidth={WIDTH}
+            css={{ stroke: color, transitionDuration: "600ms" }}
+            strokeDasharray={CIRCUMFERENCE}
+            strokeDashoffset={scale(clamp(value, min, max), [min, max], [CIRCUMFERENCE, 0])}
+          />
 
-            {hasSlider && (
-              <>
-                <Rect
-                  x={xPos}
-                  y={yPos}
-                  width={4}
-                  height={4}
-                  transform="translate(-2,-2)"
-                  fill="red"
-                />
-                <Line x1={0} x2={xPos} y1={0} y2={yPos} stroke="gray" strokeWidth={0.5} />
-                <Text fontSize={10} x={-90} y={-90} fill="red">
-                  PointerPos: x
-                  {xPos.toLocaleString("en-US", {
-                    maximumFractionDigits: 2,
-                    minimumFractionDigits: 2
-                  })}{" "}
-                  y
-                  {yPos.toLocaleString("en-US", {
-                    maximumFractionDigits: 2,
-                    minimumFractionDigits: 2
-                  })}
-                </Text>
-                <Text fontSize={10} x={0} y={10} fill="red">
-                  {targetAngle.toLocaleString("en-US", {
-                    maximumFractionDigits: 2,
-                    minimumFractionDigits: 2
-                  })}
-                  °
-                </Text>
-              </>
-            )}
-          </>
-        )}
-      </Svg>
-    </Relative>
+          {/** label marks */}
+          {calculateStepLabel(min, max, steps, 0, 360).map(({ pos }, index) => {
+            //dont print first element, if first and last el has same pos
+            if (
+              (steps % 5 === 0 || steps % 10 === 0 || steps === 0.5 || steps === 0.1) &&
+              index === 0
+            )
+              return null
+
+            //print labels
+            return (
+              <Rect
+                key={index}
+                x={0}
+                y={0}
+                width={LABEL_WIDTH}
+                height={LABEL_LENGTH}
+                transform={`rotate(${pos}) translate(-${LABEL_WIDTH / 2},-${
+                  RADIUS + LABEL_LENGTH / 2 + WIDTH + GAP
+                })`}
+                fill="gray"
+              />
+            )
+          })}
+
+          {/** Slider / Handle*/}
+          {hasSlider && (
+            <G
+              transform={`rotate(${scale(
+                snapToStep(scale(targetAngle, [0, 360], [min, max]), [0, max], steps),
+                [min, max],
+                [0, 360]
+              )}), translate(0,-86)`}
+            >
+              <Circle x={0} y={0} r={10} css={{ fill: "rgba(0,0,0,0.7)" }} />
+              {debug && (
+                <Line x1={0} y1={0} x2={0} y2={32} css={{ stroke: "purple", strokeWidth: 2 }} />
+              )}
+            </G>
+          )}
+
+          {/**Debug */}
+          {debug && (
+            <>
+              <Rect x={0} y={0} width={4} height={4} transform="translate(-2,-2)" fill="hotpink" />
+              <Line x1={-100} x2={100} y1={0} y2={0} stroke="hotpink" strokeWidth={0.5} />
+              <Line x1={0} x2={0} y1={-100} y2={100} stroke="hotpink" strokeWidth={0.5} />
+
+              {hasSlider && (
+                <>
+                  <Rect
+                    x={xPos}
+                    y={yPos}
+                    width={4}
+                    height={4}
+                    transform="translate(-2,-2)"
+                    fill="red"
+                  />
+                  <Line x1={0} x2={xPos} y1={0} y2={yPos} stroke="gray" strokeWidth={0.5} />
+                  <Text fontSize={10} x={-90} y={-90} fill="red">
+                    PointerPos: x
+                    {xPos.toLocaleString("en-US", {
+                      maximumFractionDigits: 2,
+                      minimumFractionDigits: 2
+                    })}{" "}
+                    y
+                    {yPos.toLocaleString("en-US", {
+                      maximumFractionDigits: 2,
+                      minimumFractionDigits: 2
+                    })}
+                  </Text>
+                  <Text fontSize={10} x={0} y={10} fill="red">
+                    {targetAngle.toLocaleString("en-US", {
+                      maximumFractionDigits: 2,
+                      minimumFractionDigits: 2
+                    })}
+                    °
+                  </Text>
+                </>
+              )}
+            </>
+          )}
+        </Svg>
+      </Relative>
+    </Box>
   )
 }
